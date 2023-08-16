d8223a794624f06bfb67d71f6b715bf5c8cbf4cb
funkatronics <funkatronicsmail@gmail.com>
2023-08-16 13:25:36 -0600

    init

---
+++ rpccore/src/commonMain/kotlin/com/funkatronics/networking/RpcDriver.kt
@@ -0,0 +1,37 @@
+package com.funkatronics.networking
+
+import com.funkatronics.rpccore.*
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.json.Json
+
+class Rpc20Driver(private val url: String,
+                  private val httpDriver: HttpNetworkDriver) : JsonRpcDriver {
+
+    private val json = Json {
+        encodeDefaults = true
+        ignoreUnknownKeys = true
+    }
+
+    override suspend fun <R> makeRequest(request: RpcRequest, resultSerializer: KSerializer<R>): Rpc20Response<R> =
+        httpDriver.makeHttpRequest(
+            HttpPostRequest(
+                url = url,
+                properties = mapOf("Content-Type" to "application/json; charset=utf-8"),
+                body = json.encodeToString(RpcRequest.serializer(), request)
+            )
+        ).run {
+            try {
+                json.decodeFromString(Rpc20Response.serializer(resultSerializer), this)
+            } catch (e: Exception) {
+                Rpc20Response(error = RpcError(-1, this))
+            }
+        }
+
+    internal class HttpPostRequest(
+        override val url: String,
+        override val properties: Map<String, String>,
+        override val body: String? = null
+    ) : HttpRequest {
+        override val method = "POST"
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+    }
+}
+
+rootProject.name = "RpcCore"
+include(":rpccore")
+include(":okiodriver")
+
