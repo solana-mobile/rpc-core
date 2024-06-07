12b163d0a3a9f2c1bf4ddca7b2f7e6f3949d0bc5
funkatronics <funkatronicsmail@gmail.com>
2024-06-07 09:46:06 -0600

    cleanup

--- ktordriver/src/commonMain/kotlin/com/solana/networking/KtorDriver.kt
+++ ktordriver/src/commonMain/kotlin/com/solana/networking/KtorDriver.kt
@@ -7,13 +7,11 @@ import io.ktor.http.*
 
 class KtorNetworkDriver(val httpClient: HttpClient = HttpClient()): HttpNetworkDriver {
     override suspend fun makeHttpRequest(request: HttpRequest): String =
-//        httpClient.use { client ->
-            httpClient.request(request.url) {
-                method = HttpMethod.parse(request.method)
-                request.properties.forEach { (k, v) ->
-                    header(k, v)
-                }
-                setBody(request.body)
-            }.bodyAsText()
-//        }
+        httpClient.request(request.url) {
+            method = HttpMethod.parse(request.method)
+            request.properties.forEach { (k, v) ->
+                header(k, v)
+            }
+            setBody(request.body)
+        }.bodyAsText()
 }
